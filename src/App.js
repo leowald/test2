73b170b7c6c3ab9c507b822d10af50b37a688319
Leora <leorawaldman@gmail.com>
2023-05-22 12:06:35 +0300

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from "./logo.svg";
+import Header from "./Components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <h1>This is an H4</h1>
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>This is an header</h1>;
+}
+
+export default Header;
